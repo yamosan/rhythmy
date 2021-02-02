d829ad948f2b16e303d2774c2b112a9b7d285031
yamori-masato <yamorimasato@gmail.com>
2021-02-03 00:40:03 +0900

    TriRectを実装

--- src/animations/index.ts
+++ src/animations/index.ts
@@ -1,3 +1,4 @@
 export { default as Ripple } from './Ripple'
 export { default as Bubble } from './Bubble'
 export { default as Fireworks } from './Fireworks'
+export { default as TriRect } from './TriRect'
