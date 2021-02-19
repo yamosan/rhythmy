0b7ea9f5a2e0d000338ccb98744e71347416a3fe
yamori-masato <yamorimasato@gmail.com>
2021-02-20 04:05:17 +0900

    plaerNamespaceにハンドラを追加

--- src/be/handlers/playerHandler.ts
+++ src/be/handlers/playerHandler.ts
@@ -6,6 +6,16 @@ const playerHandler = (io: Server, data: data) => {
   const nsp = io.of('/player')
   nsp.on('connection', (sock: Socket) => {
     console.log(`player[${sock.id}] connected`)
+
+    sock.on('update', (data: {track: number[]}) => {
+      try {
+        const id = players.findPlayer(sock.id).id
+        notes.replaceTrack(id, data.track)
+        console.log('LOG: notes is updated')
+      } catch(err) {
+        console.log(err)
+      }
+    })
     sock.on('disconnect', () => {
       console.log(`player[${sock.id}] disconnected`)
     })
