268951eb7448599c2173b1662a0aada525206036
yamori-masato <yamorimasato@gmail.com>
2021-02-20 03:49:17 +0900

    Notes#setObserverを使用

--- src/be/handlers/monitorHandler.ts
+++ src/be/handlers/monitorHandler.ts
@@ -4,6 +4,12 @@ import { data } from '../@types/data'
 const monitorHandler = (io: Server, data: data) => {
   const {players, notes} = data
   const nsp = io.of('/monitor')
+  // notesに更新がかかるたびにemit
+  notes.setObserver(() => {
+    nsp.emit('update', { notes: notes.data })
+    console.log('emit to monitor')
+  })
+
   nsp.on('connection', (sock: Socket) => {
     console.log(`monitor[${sock.id}] connected`)
     sock.on('disconnect', () => {
