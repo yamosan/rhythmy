22902d3f2d47b99f22f800f0e91da872e5906d18
yamori-masato <yamorimasato@gmail.com>
2021-03-03 00:26:44 +0900

    リファクタとupdateHandlerを追加

--- src/be/handlers/playerHandler.ts
+++ src/be/handlers/playerHandler.ts
@@ -4,24 +4,42 @@ import { data } from '../@types/data'
 type Nsp = TypedEmitter<Namespace, EventsRecord.PlayerEventsFromClient, EventsRecord.PlayerEventsFromServer>
 type Sock = TypedEmitter<Socket, EventsRecord.PlayerEventsFromClient, EventsRecord.PlayerEventsFromServer>
 
-const playerHandler = (io: Server, data: data) => {
-  const {players, notes} = data
+const playerHandler = (io: Server, models: data) => {
   const nsp: Nsp = io.of('/player')
   nsp.on('connection', (sock: Sock) => {
     console.log(`player[${sock.id}] connected`)
+    setup(sock, models)
+    updateHandler(sock, models)
+  })
+}
 
-    sock.on('update', (data: {track: binary[]}) => {
-      try {
-        const id = players.findPlayer(sock.id).id
-        notes.replaceTrack(id, data.track)
-        console.log('LOG: notes is updated')
-      } catch(err) {
-        console.log(err)
-      }
-    })
-    sock.on('disconnect', () => {
-      console.log(`player[${sock.id}] disconnected`)
-    })
+function setup(sock: Sock, models: data) {
+  const { players, notes } = models
+  if (players.setNewPlayer(sock.id)) {
+    try {
+      const id = players.findPlayer(sock.id).id
+      sock.emit('start', { notes: notes.data[id], id: id })
+    } catch (err) {
+      console.log(err)
+    }
+  } else {
+    // TODO: 満員のとき
+  }
+}
+
+function updateHandler(sock: Sock, models: data) {
+  const { players, notes } = models
+  sock.on('update', (data: {track: binary[]}) => {
+    try {
+      const id = players.findPlayer(sock.id).id
+      notes.replaceTrack(id, data.track)
+      console.log('LOG: notes is updated')
+    } catch(err) {
+      console.log(err)
+    }
+  })
+  sock.on('disconnect', () => {
+    console.log(`player[${sock.id}] disconnected`)
   })
 }
 
